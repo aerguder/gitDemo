e0cc2b86e09f2733c583b906a1005c9aaa7932a5
aerguder <abdullaherguder@gmail.com>
2020-08-01 12:00:02 -0400

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -3,5 +3,7 @@ public class HappyB20 {
     public static void main(String[] args) {
 
         System.out.println("Happy B20!");
+
+        System.out.println("Today is August 1st Sunday");
     }
 }
