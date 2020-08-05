d5f49d17fa0bfbc34697f7acff0a86386d10fae1
aerguder <abdullaherguder@gmail.com>
2020-08-05 16:43:21 -0400

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,6 +1,8 @@
 public class BookARoom {
-  
-  System.out.println("Today is Wednesday");
-  // This was written in remote repository  
-  
+  public static void main(String[] args) {
+
+
+    System.out.println("Today is Wednesday");
+    // This was written in remote repository
+  }
 }
